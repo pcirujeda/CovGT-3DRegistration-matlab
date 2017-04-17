b5bf63bb93c779048282db8829fb614ee58a4eed
pcirujeda <pol.cirujeda@gmail.com>
2017-04-17 20:24:09 +0200

    Added main example script runme.m

---
+++ runme.m
@@ -0,0 +1,19 @@
+addpath( genpath('./CovarianceDescriptor') );
+addpath( genpath('./GameTheory') );
+addpath( genpath('./Common') );
+addpath( genpath('./IO') );
+addpath( genpath('./KDTrees') );
+addpath( genpath('./Visualization') );
+addpath( genpath('./Samples') );
+
+% Read sample scene views
+sourceScene = OBJtoScene( 'Samples/Baboon_view1.obj' );
+targetScene = OBJtoScene( 'Samples/Baboon_view2.obj' );
+
+% Perform registration
+parameters = setupParameters();
+[R, T] = PerformRigidRegistration( sourceScene, targetScene, parameters );
+
+% Compose registered scene and store to file
+registeredScene = AppendRegisteredScenes( sourceScene, targetScene, R, T );
+SceneToOBJ( registeredScene, 'output.obj' );
